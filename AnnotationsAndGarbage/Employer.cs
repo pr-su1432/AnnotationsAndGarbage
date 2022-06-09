c5aa69ac53784740a763007314b0664db9b44793
[prasanna] <prasanna>
2022-06-09 13:54:23 +0530

    [prasanna]; Employee and employer details if annotations

---
+++ AnnotationsAndGarbage/Employer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnnotationsAndGarbage
+{
+    internal class Employer
+    {
+        int id;
+        string name;
+        public Employer(int i, string n)
+        {
+            id = i;
+            name = n;
+        }
+        [NewAttribute("Accessor","Gives value of Employer Id")]
+        public int getId()
+        {
+            return id;
+        }
+        [NewAttribute("Accessor", "Gives value of Employer Name")]
+        public string getName()
+        {
+            return name;
+        }
+    }
+}
---
+++ AnnotationsAndGarbage/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace AnnotationsAndGarbage
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            NewAttribute.AttributeDisplay(typeof(Employer));
+            Console.WriteLine();
+            NewAttribute.AttributeDisplay(typeof(Employee));
+            Console.WriteLine();
+
+        }
+    }
+}
