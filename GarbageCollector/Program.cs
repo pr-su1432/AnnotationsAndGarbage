b2b24a86bdf4a1e1d1b84b9f18e45d964229ab0f
[prasanna] <prasanna>
2022-06-09 18:37:38 +0530

    [prasanna]; Garbage collector

---
+++ GarbageCollector/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace GarbageCollection
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            long mem1 = GC.GetTotalMemory(false);
+            {
+                int[] values = new int[50000];
+                values = null;
+            }
+            long mem2 = GC.GetTotalMemory(false);
+            {
+                GC.Collect();
+            }
+            long mem3 = GC.GetTotalMemory(false);
+            {
+                Console.WriteLine(mem1);
+                Console.WriteLine(mem2);
+                Console.WriteLine(mem3);
+            }
+        }
+    }
+}
